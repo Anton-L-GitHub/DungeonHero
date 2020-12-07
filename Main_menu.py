bc166e7b05d338b5510df1c5bbf8d9f30205ba1a
mike-vestergaard <70510215+mike-vestergaard@users.noreply.github.com>
2020-12-07 15:39:39 +0100

    Update Main_menu.py
    Missing new line in show_help func

--- Main_menu.py
+++ Main_menu.py
@@ -3,7 +3,7 @@ def show_help():
     print("\nTry to find the way out before the monsters finds you!")
     print("\nYou can move up(U), down(D), left(L), or right(R).")
     print("\nYou cannot move into any space you have already been in.")
-    print("\If you box yourself in or get attached by the monster, you lose...")
+    print("\nIf you box yourself in or get attached by the monster, you lose...")
     print("\nTo show these instructions again type 'HELP'. To end the game type 'QUIT'.")
 
 #Initialization of parameters?
