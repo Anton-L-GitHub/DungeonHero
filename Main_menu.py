a8efaee3dfd65e763280adc2cc336eab0f3097bf
mike-vestergaard <70510215+mike-vestergaard@users.noreply.github.com>
2020-12-07 15:27:07 +0100

    Create Main_menu.py
    Show_help function displaybox

---
+++ Main_menu.py
@@ -0,0 +1,8 @@
+#Show basic instructions
+def show_help():
+    print("\nTry to find the way out before the monsters finds you!")
+    print("\nYou can move up(U), down(D), left(L), or right(R).")
+    print("\nYou cannot move into any space you have already been in.")
+    print("\If you box yourself in or get attached by the monster, you lose...")
+    print("\nTo show these instructions again type 'HELP'. To end the game type 'QUIT'.")
+
