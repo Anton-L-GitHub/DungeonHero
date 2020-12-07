c97241e9a123dab97d90bba9b17dc4b153c433e7
Phille Bäck <42610104+Phillodelphia@users.noreply.github.com>
2020-12-07 15:53:41 +0100

    LEO NY KOD SKRIV

--- gamemap.py
+++ gamemap.py
@@ -2,6 +2,7 @@ class GameMap:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.player = 'X'
         self.map_grid = []
     
     def create_map(self):
@@ -11,16 +12,32 @@ class GameMap:
             for _j in range(0, self.x):
         
                 x.append("-")
-            self.map_grid.insert(0, x)
+            self.map_grid.append(x)
     
     def print_map(self):
-        count = self.y
-        self.map_grid[0][0] = 'x'
+        count = 1
         for x in self.map_grid:
             print(f'{count} {x}')
-            count -= 1
+            count += 1
+    
+    def check_bound(self, x, y):
+        if (0 <= y <= self.y):
+            if (0 <= x <= self.x):
+                return True
+    
+        return False
+    
+    #temporary
+    def movement(self, x, y):
+        if self.check_bound(x, y) == False:
+            self.map_grid[x][y] = self.player
+        else:
+            print("Not a position, you donkey!")
+
+
 
 playMap = GameMap(5, 5)
 playMap.create_map()
+print(playMap.check_bound(0, 0))
 
 playMap.print_map()
