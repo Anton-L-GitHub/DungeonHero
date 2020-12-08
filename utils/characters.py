4d8766def253f9a413ab95fd85b3242a680fa72e
Anton L <70330052+Anton-L-GitHub@users.noreply.github.com>
2020-12-08 11:04:15 +0100

    add test_characters, change character

--- utils/characters.py
+++ utils/characters.py
@@ -1,73 +1,69 @@
 class Character:
+    """ Character blalbalbal """
 
-    ''' Character blalbalbal '''
-    
     def __init__(self):
-        self.initative = 0
+        self.initiative = 0
         self.health = 0
-        self.attack = 0 
-        self.agilty = 0
+        self.attack = 0
+        self.agility = 0
         self.backpack = []
         # Lägg till special-attack?
 
-    def init_char_from_json(self):
-        ''' Communicates with mapclass about player location '''
-        pass
-
-    def save_char_to_json(self):
-        ''' Communicates with mapclass about player location '''
-        pass
-    
     def movement(self):
-        ''' Communicates with mapclass about player location '''
+        """ Communicates with map-class about player location """
         pass
 
     def special_ability(self):
-        ''' A distinct ability for the character class '''
+        """ A distinct ability for the character class """
+        # Måste troligtvis utveckla utveckla stridssystem innan
         pass
 
+    def __repr__(self):
+        return self.__class__.__name__
 
 
 class Knight(Character):
-
-    ''' Knight-class blablablabla '''
+    """ Knight-class blablablabla """
 
     def __init__(self):
         super().__init__()
-        self.initative = 5
+        self.initiative = 5
         self.health = 10
-        self.attack = 6 
-        self.agilty = 4
+        self.attack = 6
+        self.agility = 4
 
     def special_ability(self):
+        # Måste troligtvis utveckla utveckla stridssystem innan 
         return 'Sheild block!'
 
 
 class Wizard(Character):
-
-    ''' Knight-class blablablabla '''
+    """ Wizard-class blablablabla """
 
     def __init__(self):
         super().__init__()
-        self.initative = 5
-        self.health = 10
-        self.attack = 6 
-        self.agilty = 4
+        self.initiative = 6
+        self.health = 4
+        self.attack = 9
+        self.agility = 5
 
     def special_ability(self):
+        """ A distinct ability for the character class """
+        # Måste troligtvis utveckla utveckla stridssystem innan
         return 'Glow!'
 
 
-class Theif(Character):
-
-    ''' Knight-class blablablabla '''
+class Thief(Character):
+    """ Thief-class blablablabla """
 
     def __init__(self):
         super().__init__()
-        self.initative = 5
-        self.health = 10
-        self.attack = 6 
-        self.agilty = 4
+        self.initiative = 7
+        self.health = 5
+        self.attack = 5
+        self.agility = 7
 
     def special_ability(self):
+        """ A distinct ability for the character class """
+        # Måste troligtvis utveckla utveckla stridssystem innan
         return 'Critical strike!'
