f6007950c95570b5ec3856b776e1ba31c02f913e
Anton L <70330052+Anton-L-GitHub@users.noreply.github.com>
2020-12-10 14:55:13 +0100

    ge

--- game_files/game.py
+++ game_files/game.py
@@ -1,38 +1,54 @@
-from utils.prompts import create_hero_prompt, map_create_prompt, map_spawn_prompt, start_menu_prompt
+from game_files.combat_system import Combat
+from utils.prompts import create_hero_prompt, map_create_prompt, map_spawn_prompt, start_menu_prompt, user_make_move
 
 """ Concerned with creating & loading games """
 
 
 class Game:
-    """ Concerned with game stuff """
+    """ Concerned with keeping track of hero, map and character """
 
     def __init__(self):
-        self.hero = ''
+        self.hero_name = ''
         self.character = None
         self.map = None
+        self.combat = Combat(self.character, self.map)
 
     def start_game(self):
         start_menu_prompt(self)
-        map_spawn_prompt(self.map)
+        next_room = self.map.make_move(self)
+        if next_room == False:
+            next_room = 
 
     def create_new_hero(self):
-        self.hero = input('Choose name: ')
+        self.hero_name = input('Choose name: ')
         self.character = create_hero_prompt()
         self.map_menu()
         self.map.print_map_grid()
-
-    def load_game(self):
-        pass
+        map_spawn_prompt(self.map)
 
     def map_menu(self):
         self.map = map_create_prompt()
         map_spawn_prompt(self.map)
 
-    def set_start_position(self):
+    def load_game(self):
         pass
 
     def build_hero_from_disk(self):
         pass
 
     def save_hero_to_disk(self):
+        pass
+
+
+class Combat_system:
+    """ Concerned with combat stuff """
+
+    def __init__(self, character, map):
+        self.map = None
+        self.character = None
+
+    def who_starts(self):
+        pass
+
+    def randomizer(self):
         pass
--- utils/prompts.py
+++ utils/prompts.py
@@ -4,9 +4,10 @@ from game_files.characters import Knight, Thief, Wizard
 """ Concerned displaying prompts in terminal """
 
 START_MENU = '\n1: New game\n2: Load game\n0: Exit \n> '
-CHARACTER_CHOISE = '\nChoose character:\n1: Knight\n2: Wizard\n3: Thief\n---\n0: Exit \n> '
+CHARACTER_CHOISE = '\nChoose character:\n1: Knight\n2: Wizard\n3: Thief\n--- \n> '
 MAP_MENU = '\nMap Difficulty: \n1: Small \n2: Medium \n2: Large \n---\n0: Exit \n> '
-MAP_SPAWN = '\nSpawn: \n1: Bottom left \n2: Botton right \n\n3: Top Left \n4: Top right\n---\n0: Exit \n> '
+MAP_SPAWN = '\nSpawn: \n1: Bottom left \n2: Botton right \n\n3: Top Left \n4: Top right\n--- \n> '
+USER_MOVE = "It's time!\nWhere do you want to go? \nUp(W)  Down(S) \nLeft(A) Right(D)"
 
 
 def start_menu_prompt(game):
@@ -34,7 +35,7 @@ def create_hero_prompt():
 
 def map_create_prompt():
     user_input = input(MAP_MENU)
-    while user_input != '0':
+    while user_input != '!QUIT':
         if user_input == '1':
             return create_map_instance('small')
         elif user_input == '2':
@@ -46,7 +47,7 @@ def map_create_prompt():
 
 def map_spawn_prompt(map):
     user_input = input(MAP_SPAWN)
-    while user_input != '0':
+    while user_input != '!QUIT':
         if user_input == '1':
             return map.set_start_position('b-l')
         elif user_input == '2':
@@ -55,4 +56,19 @@ def map_spawn_prompt(map):
             return map.set_start_position('t-l')
         elif user_input == '3':
             return map.set_start_position('t-r')
+        user_input = input(MAP_MENU)
+
+
+def user_make_move():
+    user_input = input(USER_MOVE)
+    while user_input != '!QUIT':
+        if user_input == 'W':
+            return map.set_start_position('W')
+        elif user_input == 'S':
+            return map.set_start_position('S')
+        elif user_input == 'A':
+            return map.set_start_position('A')
+        elif user_input == 'D':
+            return map.set_start_position('D')
+
         user_input = input(MAP_MENU)
