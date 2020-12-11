d3420d9be5d295114cdc415c50c4e2889fd00851
Phille Bäck <42610104+Phillodelphia@users.noreply.github.com>
2020-12-11 14:55:12 +0100

    Conitnue in my legacy

--- data/database/database.py
+++ data/database/database.py
@@ -6,18 +6,24 @@ from game_files import characters
 json_file = 'data/database/data.json'
 
 
-print("Test test")
-
 def disc_get(json_file):
     """ Returns a list of all instances in db-file """
     with open(json_file, 'r') as file:
         return json.load(file)
 
 def disc_save_character(player):
-    newPlayer = player.__dict__
-    print(newPlayer)
+    print(player.__dict__)
+    json_path = f'data/database/characters/character_{player.name}.json'
+    with open(json_path, 'w+') as f:
+        json.dump(player.__dict__, f, indent=4)
+
 
 def disc_save_map(list_of_dicts):
     """ Takes list of instances attributes in form of a py-dict and writes it to a json file """
     with open(json_file, 'w') as file:
         json.dump(list_of_dicts, file)
+
+newPlayer = characters.Knight()
+newPlayer.name = 'Pelle'
+
+disc_save_character(newPlayer)
