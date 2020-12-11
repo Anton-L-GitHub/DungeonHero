63256f44e7336f1bc5cb751fe72ed0d173e941fe
Phille Bäck <42610104+Phillodelphia@users.noreply.github.com>
2020-12-11 19:03:32 +0100

    added a bit of code

--- data/database/database.py
+++ data/database/database.py
@@ -1,5 +1,7 @@
 import json
-from game_files import characters
+from game_files import characters, gamemap
+from os import listdir, remove
+from os.path import isfile, join
 
 """ Concerned with storing and retrieving media-objects from a JSON-file. """
 
@@ -12,11 +14,24 @@ def disc_get(json_file):
         return json.load(file)
 
 def disc_save_character(player):
-    print(player.__dict__)
     json_path = f'data/database/characters/character_{player.name}.json'
-    with open(json_path, 'w+') as f:
-        json.dump(player.__dict__, f, indent=4)
+    try:
+        with open(json_path, 'w+') as f:
+            json.dump(player.__dict__, f, indent=4)
+    except FileNotFoundError as F:
+        print(F)
+        
 
+def disc_save_progress(player, map):
+    json_path_orig = f'data/database/characters'
+    onlyfiles = [f for f in listdir(json_path_orig) if isfile(join(json_path_orig, f))]
+    tempString = f'character_{player.name}.json'
+    if tempString in onlyfiles:
+        json_path_orig += f'/{tempString}'
+        remove(json_path_orig)
+    else:
+        print("Couldn't find file.")
+    # json_path = f'data/database/characters_ongoing/character_{player.name}.json'
 
 def disc_save_map(list_of_dicts):
     """ Takes list of instances attributes in form of a py-dict and writes it to a json file """
@@ -24,6 +39,8 @@ def disc_save_map(list_of_dicts):
         json.dump(list_of_dicts, file)
 
 newPlayer = characters.Knight()
-newPlayer.name = 'Pelle'
+newPlayer.name = 'Stor pelle'
+tempMap = gamemap.create_map_instance('medium')
+tempMap.set_start_position('t-l')
 
-disc_save_character(newPlayer)
+disc_save_progress(newPlayer, tempMap)
