eae48cec704ac0df3145d079cdacbe5634dc7bdc
Phille Bäck <42610104+Phillodelphia@users.noreply.github.com>
2020-12-14 11:58:39 +0100

    Added load character function

--- data/database/database.py
+++ data/database/database.py
@@ -1,9 +1,11 @@
 import json
+from types import SimpleNamespace
 from game_files import characters, gamemap
 from os import listdir, remove
 from os.path import isfile, join
 
 """ Concerned with storing and retrieving media-objects from a JSON-file. """
+
 
 json_file = 'data/database/data.json'
 
@@ -45,15 +47,33 @@ def disc_save_progress(player, player_map):
         file.write(to_json(mapDict))
         
 
+def disc_load_character(player_name):
+    json_path = f'data/database/characters/character_{player_name}.json'
+
+    with open(json_path, 'r') as f:
+        player_dict = json.load(f)
+
+    newPlayer = characters.Character()
+    json.loads(json.dumps(player_dict['player']), object_hook=newPlayer.parse_data) 
+
+    return newPlayer
+
+
 def disc_save_map(list_of_dicts):
     """ Takes list of instances attributes in form of a py-dict and writes it to a json file """
     with open(json_file, 'w') as file:
         json.dump(list_of_dicts, file)
 
-# newPlayer = characters.Knight()
-# newPlayer.name = 'Pelle'
-# tempMap = gamemap.create_map_instance('medium')
-# tempMap.set_start_position('t-l')
+def create_dump():
+    newPlayer = characters.Knight()
+    # newPlayer2 = characters.Knight()
+    newPlayer.name = 'Mike'
+    # newPlayer2.name = 'Leo'
+    # tempMap = gamemap.create_map_instance('medium')
+    # tempMap.set_start_position('t-l')
 
-# disc_save_character(newPlayer)
-# disc_save_progress(newPlayer, tempMap)
+    # disc_save_progress(newPlayer, tempMap)
+    test = disc_load_character(newPlayer.name)
+    # test2 = disc_load_character(newPlayer2.name)
+    print(f'{test.name}')
+
